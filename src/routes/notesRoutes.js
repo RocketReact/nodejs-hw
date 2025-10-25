98d8ce716e77485d691963fc5f86a3f5d2fde1ab
Oleg Shapoval <olegkazhan@gmail.com>
2025-10-25 19:59:22 +0300

    id validate

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -7,17 +7,20 @@ import {
   updateNote,
 } from '../controllers/notesController.js';
 import { celebrate } from 'celebrate';
-import { getAllNotesSchema } from '../validations/noteValidation.js';
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+} from '../validations/noteValidation.js';
 
 const router = Router();
 
 //get all notes
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 //get notes by id
-router.get('/notes/:noteId', getNoteById);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 
 router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNote);
-router.patch('/notes/:noteId', updateNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+router.patch('/notes/:noteId', celebrate(noteIdSchema), updateNote);
 
 export default router;
