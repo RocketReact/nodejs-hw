97bbcae649c814ca1cde6189601002ddbe0ef9ed
Oleg Shapoval <olegkazhan@gmail.com>
2025-10-29 22:18:11 +0200

    user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ import { User } from '../models/user.js';
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import { createSession, setSessionCookies } from '../services/auth.js';
+import { Session } from '../models/session.js';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -20,4 +21,20 @@ export const registerUser = async (req, res, next) => {
   const newSession = await createSession(newUser._id);
   setSessionCookies(res, newSession);
   res.status(201).json(newUser);
+};
+export const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(createHttpError(401, 'Invalid credential'));
+  }
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    return next(createHttpError(401, 'Invalid credential'));
+  }
+
+  await Session.deleteOne({ userId: user._id });
+  const newSession = await createSession(user._id);
+  setSessionCookies(res, newSession);
+  res.status(200).json(user);
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUserSchema } from '../validations/authValidation.js';
+import {
+  loginUser,
+  registerUserSchema,
+} from '../validations/authValidation.js';
 import { registerUser } from '../controllers/authController.js';
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUser), loginUser);
