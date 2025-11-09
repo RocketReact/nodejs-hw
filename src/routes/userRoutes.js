97b2db9fb40b8e609d16aa2c2181339500b29f90
Oleg Shapoval <olegkazhan@gmail.com>
2025-11-09 18:51:30 +0200

    fix

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
-import router from './notesRoutes.js';
 import { updateUserAvatar } from '../controllers/userController.js';
 import { authenticate } from '../middleware/authenticate.js';
 import { upload } from '../middleware/multer.js';
+import { Router } from 'express';
 
+const router = Router();
 router.patch(
   '/users/me/avatar',
   authenticate,
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -10,6 +10,6 @@ const transporter = nodemailer.createTransport({
     pass: SMTP_PASSWORD,
   },
 });
-export const sendMail = async (options) => {
-  return await transporter.sendMail({ options });
+export const sendEmail = async (options) => {
+  return await transporter.sendMail(options);
 };
